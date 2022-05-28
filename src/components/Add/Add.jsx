b4d8d00c797725027ddbb3402c09c0ada7e47be8
snowleopardAlexa <alexaslomski@gmail.com>
2022-05-28 19:56:56 -0400

    store api results and add map - display results on the screen, check for errors with if statement

--- src/components/Add/Add.jsx
+++ src/components/Add/Add.jsx
@@ -4,18 +4,28 @@ import { useState } from "react";
 
 const Add = () => {
   const [query, setQuery] = useState("")
+  // store api result - return the array of results useState([] - array of results)
+  const [results, setResults] = useState([])
 
   const onChange = (e) => {
     e.preventDefault();
 
     setQuery(e.target.value);
 
+    // fetch api from tmdb api via promises
     fetch(
       `https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_MOVIES_KEY}&language=en-US&page=1&include_adult=false&query=${e.target.value}`
     )
       .then((res) => res.json())
       .then((data) => {
-        console.log(data)
+        // check for errors
+        if(!data.errors) {
+          // return api to the state
+          setResults(data.results)
+        } else {
+          // if there is no errors - set results to an empty array
+          setResults([])
+        }
       });
   };
 
@@ -29,7 +39,16 @@ const Add = () => {
           onChange={onChange}
         />
       </div>
-     
+      {results.length > 0 && (
+        <ul className="results">
+          {results.map(movie => (
+            <li>
+              {movie.title}
+            </li>
+          ))}
+        </ul>
+        )
+      }  
       </div>
   );
 };
