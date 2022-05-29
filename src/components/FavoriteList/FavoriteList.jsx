1a81f2884bb2cfa7595e3464be8c6e86845ca2e7
snowleopardAlexa <alexaslomski@gmail.com>
2022-05-29 18:49:10 -0400

    add no movies found

--- src/components/FavoriteList/FavoriteList.jsx
+++ src/components/FavoriteList/FavoriteList.jsx
@@ -1,23 +1,27 @@
-import './FavoriteList.css'
-import { useContext } from 'react'
-import { GlobalContext } from '../../context/GlobalState'
-import MovieCard from '../MovieCard/MovieCard'
+import "./FavoriteList.css";
+import { useContext } from "react";
+import { GlobalContext } from "../../context/GlobalState";
+import MovieCard from "../MovieCard/MovieCard";
 
 const FavoriteList = () => {
-const { favoritelist } = useContext(GlobalContext)
+  const { favoritelist } = useContext(GlobalContext);
 
   return (
     <div className="favoritelist__container">
-    <div className="header">
-      <h2 style={{textAlign: 'center'}}>Favorite List</h2>
-    </div>  
-    <div className="movie__grid">
-     {favoritelist.map((movie) => (
-       <MovieCard movie={movie} type="favoritelist" />
-     ))}
+      <div className="header">
+        <h2 style={{ textAlign: "center" }}>Favorite List</h2>
+      </div>
+      {favoritelist.length > 0 ? (
+        <div className="movie__grid">
+          {favoritelist.map((movie) => (
+            <MovieCard movie={movie} type="favoritelist" />
+          ))}
+        </div>
+      ) : (
+        <h2 className="no-movies">No movies found, add some!</h2>
+      )}
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default FavoriteList
+export default FavoriteList;
