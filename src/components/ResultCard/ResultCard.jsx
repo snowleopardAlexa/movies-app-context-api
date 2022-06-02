74d4c403acd3e6b0c163a293d59198ae09c5d306
snowleopardAlexa <alexaslomski@gmail.com>
2022-06-01 20:25:48 -0400

    add watched button disabled

--- src/components/ResultCard/ResultCard.jsx
+++ src/components/ResultCard/ResultCard.jsx
@@ -5,50 +5,68 @@ import { GlobalContext } from "../../context/GlobalState";
 // pass movie object as props so we can access it by destructuring
 const ResultCard = ({ movie }) => {
   // access to add movie that we created in context
-  const { addMovieToFavoritelist, favoritelist } = useContext(GlobalContext);
+  const { addMovieToFavoritelist, addMovieToWatched, favoritelist, watched } =
+    useContext(GlobalContext);
 
   // store movies - check for object
-  let storedMovie = favoritelist.find(o => o.id === movie.id);
+  let storedMovie = favoritelist.find((o) => o.id === movie.id);
+  // store movies in watched
+  let storedMovieWatched = watched.find((o) => o.id === movie.id);
 
   // we don't want to store movies - button disabled - movie already stored
-  const favoritelistDisabled = storedMovie ? true : false;
+  const favoritelistDisabled = storedMovie
+    ? true
+    : storedMovieWatched
+    ? true
+    : false
+
+  // button disabled on add to watched 
+  const watchedDisabled = storedMovieWatched ? true : false   
 
   return (
     <div className="result__container">
       <div className="result__card">
-      <div className="poster__wrapper">
-        {movie.poster_path ? (
-          // backticks with temple literals
-          <img
-            src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
-            alt={`${movie.title} Poster`}
-          />
-        ) : (
-          // if there is no poster
-          <div className="fake__poster"></div>
-        )}
-      </div>
-      <div className="info">
-        <div className="header">
-          <h3 className="title">{movie.title}</h3>
-          <h4 className="release-date">
-            {/* check for error in release date  */}
-            {movie.release_date
-              ? movie.release_date.substring(0, 4)
-              : "Not Disclosed"}
-          </h4>
+        <div className="poster__wrapper">
+          {movie.poster_path ? (
+            // backticks with temple literals
+            <img
+              src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
+              alt={`${movie.title} Poster`}
+            />
+          ) : (
+            // if there is no poster
+            <div className="fake__poster"></div>
+          )}
         </div>
-        <div className="favorite">
-          {/* trigger add movie to favorite list on btn - send movie object accessed in the props */}
-          <button 
-            disabled={favoritelistDisabled}
-            className="btn" 
-            onClick={() => addMovieToFavoritelist(movie)}
-           >
-            Add to Favorites
-          </button>
+        <div className="info">
+          <div className="header">
+            <h3 className="title">{movie.title}</h3>
+            <h4 className="release-date">
+              {/* check for error in release date  */}
+              {movie.release_date
+                ? movie.release_date.substring(0, 4)
+                : "Not Disclosed"}
+            </h4>
+          </div>
+          <div className="favorite">
+            {/* trigger add movie to favorite list on btn - send movie object accessed in the props */}
+            <button
+              disabled={favoritelistDisabled}
+              className="btn"
+              onClick={() => addMovieToFavoritelist(movie)}
+            >
+              Add to Favorites
+            </button>
+            {/* trigger add movie to watched on btn - send movie object accessed in the props */}
+            <button
+              disabled={watchedDisabled}
+              className="btn"
+              onClick={() => addMovieToWatched(movie)}
+            >
+              Add to Watched
+            </button>
+          </div>
         </div>
-      </div>
       </div>
     </div>
   );
